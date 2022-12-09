7c4c0ca47ca71e548a18e5f8c6514c380993d6af
frederic.pitteloud@epfl.ch <frederic.pitteloud@epfl.ch>
2022-12-09 16:26:15 +0100

    gitignore and main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,5 @@
-provider   "azurerm"   { 
-   version   =   "= 2.0.0" 
-   features   {} 
- }
- resource   "azurerm_resource_group"   "rg"   { 
-   name   =   "iaac-opdo" 
-   location   =   "Switzerland North" 
- }
+resource   "azurerm_resource_group"   "rg"   { 
+  name   =   "iaac-opdo" 
+  location   =   "Switzerland North" 
+}
 
