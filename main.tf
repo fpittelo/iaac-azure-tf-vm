51f2f5a99f84703c5495694f6ebddd853e64b317
frederic.pitteloud@epfl.ch <frederic.pitteloud@epfl.ch>
2022-12-11 14:02:14 +0100

    variables + network

--- main.tf
+++ main.tf
@@ -1,12 +1,19 @@
 resource   "azurerm_resource_group"   "rg"   { 
-  name   =   "IAAC-AZURE-OPDO" 
-  location   =   "Switzerland North" 
+  name   =   var.resource_group_name 
+  location   =   var.resource_group_location 
 }
 
 resource "azurerm_log_analytics_workspace" "rg" {
-  name                = "iaac-azure-opdo-logs"
+  name                = var.azurerm_log_analytics_workspace
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
+}
+
+resource "azurerm_virtual_network" "opdo-net" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  default = "IAAC-AZURE-OPDO"
+}
+
+variable "resource_group_location" {
+  default = "Switzerland North"
+}
+
+variable "azurerm_log_analytics_workspace" {
+  default = "OPDO-LOGS"
+}
+
+variable "prefix" {
+  default = "opdo"
+}
