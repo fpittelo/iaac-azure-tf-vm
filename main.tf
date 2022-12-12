7d9d5e74c7aa89c18d70fe1817fc98cc1c2f7699
frederic.pitteloud@gmail.com <frederic.pitteloud@gmail.com>
2022-12-12 17:25:14 +0100

    update of all files

--- main.tf
+++ main.tf
@@ -1,43 +1,86 @@
+
 resource   "azurerm_resource_group"   "rg"   { 
-  name   =   var.resource_group_name 
-  location   =   var.resource_group_location 
+  name                =   var.rg_name 
+  location            =   var.rg_location 
 }
 
-resource "azurerm_log_analytics_workspace" "rg" {
-  name                = var.azurerm_log_analytics_workspace
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "time_sleep" "wait_rg_creation" {
+  depends_on = [azurerm_resource_group.rg]
+  create_duration = "90s"
+}
+
+resource "azurerm_log_analytics_workspace" "logwks" {
+  name                = var.log_analytics_wks
+  location            = var.rg_location
+  resource_group_name = var.rg_name
   sku                 = "PerGB2018"
   retention_in_days   = 30
+  depends_on          = [azurerm_resource_group.rg]
 }
 
 resource "azurerm_network_security_group" "opdo" {
-  name                = "opdo-sec-group"
-  location            = var.resource_group_location
-  resource_group_name = var.resource_group_name  
+  name                = var.log_analytics_wks
+  location            = var.rg_location
+  resource_group_name = var.rg_name
+  depends_on          = [azurerm_resource_group.rg]
 }
 
-resource "azurerm_virtual_network" "opdo-net" {
-  name                = "${var.prefix}-network"
+resource "azurerm_virtual_network" "opdo_vnet" {
+  name                = var.opdo_virtual_net
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.rg_location
+  resource_group_name = var.rg_name
   dns_servers         = ["8.8.8.8", "8.8.4.4"]
-  
-  subnet {
-    name           = "opdo-subnet-1"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  subnet {
-    name           = "opdo-subnet-2"
-    address_prefix = "10.0.2.0/24"
-    security_group = azurerm_network_security_group.opdo.id
-  }
-
-  tags = {
-    environment = "Production"
-    owner       = "VPO-DSI-ISGOV-GE"
-  }
+  depends_on          = [azurerm_resource_group.rg]
 }
 
+resource "time_sleep" "wait_vnet_creation" {
+  depends_on = [azurerm_virtual_network.opdo_vnet]
+  create_duration = "90s"
+}
+
+# Create subnet internal
+resource "azurerm_subnet" "opdo_subnet_int" {
+  name                 = var.opdo_subnet_int
+  resource_group_name  = var.rg_name
+  virtual_network_name = var.opdo_virtual_net
+  address_prefixes     = ["10.0.1.0/24"]
+  depends_on           = [azurerm_virtual_network.opdo_vnet]
+}
+
+# Create subnet public
+resource "azurerm_subnet" "opdo_subnet_pub" {
+  name                 = var.opdo_subnet_pub
+  resource_group_name  = var.rg_name
+  virtual_network_name = var.opdo_virtual_net
+  address_prefixes     = ["10.0.2.0/24"]
+  depends_on           = [azurerm_virtual_network.opdo_vnet]
+}
+
+# Create private IP
+resource "azurerm_public_ip" "opdo_vm01_intip" {
+  name                = var.opdo_vm01_intip
+  location            = var.rg_location
+  resource_group_name = var.rg_name
+  allocation_method   = "Dynamic"
+}
+
+# Create public IP
+resource "azurerm_public_ip" "opdo_vm01_pubip" {
+  name                = var.opdo_vm01_pubip
+  location            = var.rg_location
+  resource_group_name = var.rg_name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "opdo_vm01_nic" {
+  name                = var.opdo_vm01_nic
+  location            = var.rg_location
+  resource_group_name = var.rg_name
+
+  ip_configuration {
+    name                          = var.opdo_vm01_ipcfg
+    subnet_id                     = azurerm_subnet.opdo_subnet_int.id
+    private_ip_address_allocation = "Dynamic"
+    }
+}
